ef69053e6dc01a49ff9006f25a648c9a0bd5388f
Anonio Salazar <antonio.salazar.devops@gmail.com>
2023-01-29 04:29:25 -0600

    Pipelines

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
 public class hello_world {
   public static void main(String args[]){
     System.out.println("Hello, Wonderful World!");
-    System.out.println("This is great, I love DevOps!");
+    //System.out.println("This is great, I love DevOps!");
   }
 };
